acf15b856808e788eaad38f26436a8164906c637
Daniel Pittman <dpdanpittman@gmail.com>
2023-08-12 21:04:14 -0600

    remove footer

--- src/App.tsx
+++ src/App.tsx
@@ -36,26 +36,6 @@ function App() {
       <main>
         <Outlet context={contract} />
       </main>
-
-      <footer>
-        <small>
-          <p>
-            Contract source code:{" "}
-            <a href="https://github.com/jeremyfee/cosmwasm-chess/tree/0.4.1">
-              https://github.com/jeremyfee/cosmwasm-chess/tree/0.4.1
-            </a>
-            <br />
-            UI source code:{" "}
-            <a href="https://github.com/jeremyfee/cosmwasm-chess-ui">
-              https://github.com/jeremyfee/cosmwasm-chess-ui
-            </a>
-          </p>
-          <p>
-            Created by:{" "}
-            <Address address="juno1qzmu3y33vhwhexwwtctp7e3fn20qnfphy3f04w" />
-          </p>
-        </small>
-      </footer>
     </div>
   );
 }
